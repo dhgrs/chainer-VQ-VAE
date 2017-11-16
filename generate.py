71e27257b32d885db524330c39ddc234fc89605d
dhgrs <d.higurashi@gmail.com>
2017-11-16 11:48:25 +0900

    add generate

---
+++ generate.py
@@ -0,0 +1,22 @@
+import sys
+
+import numpy as np
+import librosa
+import chainer
+
+import models
+from utils import mu_law
+from utils import Preprocess
+import opt
+
+model = models.VAE()
+chainer.serializers.load_npz(sys.argv[1], model)
+
+n = 1
+x = np.expand_dims(Preprocess(
+    opt.data_format, opt.sr, opt.mu, opt.sr * 3, False)(sys.argv[2])[0], 0)
+output = model.generate(x, Preprocess(
+    opt.data_format, opt.sr, opt.mu, opt.sr * 3)(sys.argv[2])[2])
+wave = mu_law(opt.mu).itransform(output)
+np.save('result.npy', wave)
+librosa.output.write_wav('result.wav', wave, opt.sr)
