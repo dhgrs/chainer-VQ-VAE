ed6fdeb9d78d636b0149307851c3896a5d4cd998
dhgrs <d.higurashi@gmail.com>
2017-11-16 11:17:00 +0900

    add parameters

--- opt.py
+++ opt.py
@@ -1,11 +1,27 @@
-batchsize = 1
+# parameters of training
+batchsize = 32
 lr = 2e-4
-
 trigger = (100000, 'iteration')
 report_interval = (1000, 'iteration')
 
+# parameters of dataset
 root = 'VCTK-Corpus'
 data_format = 'wav'
 sr = 8000
 mu = 256
 length = 8192
+
+# parameters of VQ
+d = 256
+k = 512
+
+# parameters of Decoder(WaveNet)
+n_loop = 3
+n_layer = 10
+n_filter = 2
+n_channel1 = 32
+n_channel2 = 16
+n_channel3 = 512
+
+# parameters of losses
+beta = 0.25
