253f735810adf84f459d34b3eb862a748c8f6b1b
dhgrs <d.higurashi@gmail.com>
2017-11-20 14:34:38 +0900

    update generate
    load parameters in opt

--- generate.py
+++ generate.py
@@ -4,12 +4,13 @@ import numpy as np
 import librosa
 import chainer
 
-import models
+from models import VAE
 from utils import mu_law
 from utils import Preprocess
 import opt
 
-model = models.VAE()
+model = VAE(opt.d, opt.k, opt.n_loop, opt.n_layer, opt.n_filter, opt.mu,
+            opt.n_channel1, opt.n_channel2, opt.n_channel3, opt.beta, True)
 chainer.serializers.load_npz(sys.argv[1], model)
 
 n = 1
