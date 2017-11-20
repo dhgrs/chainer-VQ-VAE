92c32877f6372667d13c71b2a97c601f3cc946b9
dhgrs <d.higurashi@gmail.com>
2017-11-20 16:40:51 +0900

    add result
    add reconstruct result after 5000 iterations

--- opt.py
+++ opt.py
@@ -1,27 +1,27 @@
 # parameters of training
-batchsize = 32
+batchsize = 8
 lr = 2e-4
 trigger = (100000, 'iteration')
-report_interval = (1000, 'iteration')
+report_interval = (5000, 'iteration')
 
 # parameters of dataset
 root = 'VCTK-Corpus'
 data_format = 'wav'
-sr = 8000
+sr = 16000
 mu = 256
-length = 8192
+length = 16000
 
 # parameters of VQ
 d = 256
-k = 512
+k = 384
 
 # parameters of Decoder(WaveNet)
 n_loop = 3
 n_layer = 10
 n_filter = 2
-n_channel1 = 32
-n_channel2 = 16
-n_channel3 = 512
+n_channel1 = 64
+n_channel2 = 32
+n_channel3 = 256
 
 # parameters of losses
 beta = 0.25
